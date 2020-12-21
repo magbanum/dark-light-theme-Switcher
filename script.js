9282d2ee377b6794e6c8c203536342b38f2aeb7c
magbanum <nighotshantanu@gmail.com>
2020-12-21 20:25:39 +0530

    Working code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-  const btn = document.querySelector(".switch__label");
 
-    btn.addEventListener("click", function () {
-      document.body.classList.toggle("dark-theme");
-    });
+const btn = document.querySelector(".switch__label");
+
+btn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
+});
